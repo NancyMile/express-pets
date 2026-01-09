541105860b302338fd0a6b65496bc8e1caf3a2c7
Nancy <smithnancy@outlook.com>
2026-01-09 22:17:16 +1100

    Programming sending emails

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,3 +1,4 @@
+const nodemailer = require("nodemailer")
 const { ObjectId } = require("mongodb")
 const sanitizeHtml = require("sanitize-html")
 const petsCollection = require("../db").db().collection("pets")
@@ -32,6 +33,38 @@ if(!doesPetExist){
     comment:sanitizeHtml(req.body.comment, sanitizeOptions),
   }
 
-  console.log(req.body);
+  console.log(ourObject)
+  // Looking to send emails in production? Check out our Email API/SMTP product!
+  var  transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: process.env.MAILTRAPUSERNAME,
+      pass: process.env.MAILTRAPPASSWORD
+    }
+  })
+
+  //email to the client
+  transport.sendMail({
+    to: ourObject.email,
+    from:"pets@test.com",
+    subject:`Thank you for for your interest in ${doesPetExist.name}`,
+    html:`<h3 style="color:purple; font-size:30px; font-weight:normal;">${ourObject.name}</h3>
+    <p> We appreciate your interest in ${doesPetExist.name}</p>
+    <p><em>${ourObject.comment}</em></p>`
+  })
+
+  //email to manager
+  transport.sendMail({
+    to: "petsadmin@test.com",
+    from:"pets@test.com",
+    subject:`Someone is interested in ${doesPetExist.name}`,
+    html:`<h3 style="color:purple; font-size:30px; font-weight:normal;">${ourObject.name}</h3>
+    <p> Name: ${ourObject.name}<br>
+    Pet: ${doesPetExist.name}<br>
+    Email: ${ourObject.email}<br>
+    Message: ${ourObject.comment}
+    </p>`
+  })
   res.send("Thanks for sending data to us")
 }
