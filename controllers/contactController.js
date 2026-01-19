de95a7d6c0d24a3f4c157751a50d07bb703de42a
Nancy <smithnancy@outlook.com>
2026-01-19 16:02:55 +1100

    Saving contacts to database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,6 +3,7 @@ const validator = require("validator")
 const { ObjectId } = require("mongodb")
 const sanitizeHtml = require("sanitize-html")
 const petsCollection = require("../db").db().collection("pets")
+const contactsCollection = require("../db").db().collection("contacts")
 
 const sanitizeOptions = {
   allowedTags: [],
@@ -16,7 +17,19 @@ exports.submitContact = async function(req, res,next){
     return res.json({message:"sorry"})
   }
 
-  if(validator.isEmail(req.body.email)){
+  if(typeof req.body.name != "string"){
+    req.body.name = ""
+  }
+
+  if(typeof req.body.email != "string"){
+    req.body.email = ""
+  }
+
+  if(typeof req.body.comment != "string"){
+    req.body.comment = ""
+  }
+
+  if(!validator.isEmail(req.body.email)){
     console.log("Invalid email")
     return res.json({message:"sorry"})
   }
@@ -26,7 +39,8 @@ exports.submitContact = async function(req, res,next){
     return res.json({message:"sorry"})
   }
 
-const doesPetExist =  await petsCollection.findOne({_id: new ObjectId(req.body.petId)})
+req.body.petId = new ObjectId(req.body.petId)
+const doesPetExist =  await petsCollection.findOne({_id: req.body.petId})
 
 if(!doesPetExist){
     console.log("Pet doesn't exists")
@@ -34,6 +48,7 @@ if(!doesPetExist){
 }
 
   const ourObject= {
+    petId: req.body.petId,
     name:sanitizeHtml(req.body.name, sanitizeOptions),
     email:sanitizeHtml(req.body.email, sanitizeOptions),
     comment:sanitizeHtml(req.body.comment, sanitizeOptions),
@@ -74,7 +89,9 @@ if(!doesPetExist){
       </p>`
     })
 
-    await Promise.all([promise1,promise2])
+    const promise3 = await contactsCollection.insertOne(ourObject)
+
+    await Promise.all([promise1,promise2,promise3])
 
   }catch(err){
     next(err)
